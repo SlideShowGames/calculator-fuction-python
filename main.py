fea49a4f4193ed187aae9e820d561b3eac4dbe4f
Bradley Stratton <108354503+SlideShowGames@users.noreply.github.com>
2022-09-28 16:41:17 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+def calculator():
+    print("current mode addition")
+    number1 = input("enter first number")
+
+
+number2 = input("enter second number")
+vsum = float(number1) + float(number2)
+print(vsum)
+print("mode now subtraction")
+number3 = input("enter first number")
+number4 = input("enter second number")
+vsum2 = float(number3) - float(number4)
+print(vsum2)
+print("mode now division ")
+number5 = input("enter first number")
+number6 = input("enter second number")
+vsum3 = float(number5) / float(number6)
+print(vsum3)
+print("mode now multiplication")
+number7 = input("enter first number")
+number8 = input("enter second number")
+vsum4 = float(number7) * float(number8)
+print(vsum4)
+
+calculator()
+
+
+
+#Thank you for the calculator to your code if you want more sub packages go to
+# https://github.com/SlideShowGames/SlideShowGames and also check out my YouTube https://www.youtube.com/channel/UCECRYbDpMr5aMyxhnS3ESGg
